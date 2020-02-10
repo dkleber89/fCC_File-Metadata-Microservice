7a90dcde09c92173dab74bef4fd8b96e697b1f3e
dkleber89 <dkleber89@gmail.com>
2020-02-10 21:21:32 +0100

    Prepare Project

---
+++ server.mjs
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+
+import { gDirname } from './utils/utilityFunctions.mjs';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${gDirname(import.meta.url)}/views/index.html`);
+});
+
+app.get('/hello', (req, res) => {
+  res.json({ greetings: 'Hello, API' });
+});
+
+app.post('/api/fileanalyse', (req, res) => {});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Node.js listening on Port ${process.env.PORT || 3000}`);
+});
